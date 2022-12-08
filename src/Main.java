f2a69f3634f2c1d10653059a4816124eaf2c0255
Dmitriy Ogorodnikov <diminskiy_4791@mail.ru>
2022-12-08 07:33:37 +0300

    Доработка

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class Main {
+    static  int maxNumberLettersSurname = 6;// максимальное колличество слов в фамилии
     public static void main(String[] args) {
         ArrayList<Person> persons = new ArrayList<>();
         persons.add(new Person("Ваня", "Иванов", 30));
@@ -11,14 +12,17 @@ public class Main {
         persons.add(new Person("Антон", "Суворов", 15));
         persons.add(new Person("Андрей", "Васенин", 13));
         persons.add(new Person("Миша", "Орлов", 55));
-        int m = 6;// максимальное колличество слов в фамилии
         Comparator<Person> comparison = (o1, o2) -> {
-            if (o1.getSurname().length() >= m && o2.getSurname().length() < m) return -1;
-            else if (o1.getSurname().length() < m && o2.getSurname().length() >= m) return 1;
-            else if (o1.getSurname().length() >= m && o2.getSurname().length() >= m)
+            if (o1.getSurname().length() >= maxNumberLettersSurname && o2.getSurname().length() < maxNumberLettersSurname)
+                return -1;
+            else if (o1.getSurname().length() < maxNumberLettersSurname && o2.getSurname().length() >= maxNumberLettersSurname)
+                return 1;
+            else if (o1.getSurname().length() >= maxNumberLettersSurname && o2.getSurname().length() >= maxNumberLettersSurname)
                 return Integer.compare(o2.getAge(), o1.getAge());
-            else if (o1.getSurname().length() > o2.getSurname().length()) return -1;
-            else if (o1.getSurname().length() < o2.getSurname().length()) return 1;
+            else if (o1.getSurname().length() > o2.getSurname().length())
+                return -1;
+            else if (o1.getSurname().length() < o2.getSurname().length())
+                return 1;
             else return Integer.compare(o2.getAge(), o1.getAge());
         };
         persons.sort(comparison);
