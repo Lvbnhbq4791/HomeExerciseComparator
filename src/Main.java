e312bc362bf44fc1120a71d573677475c7f049a5
Dmitriy Ogorodnikov <diminskiy_4791@mail.ru>
2022-12-12 19:49:44 +0300

    Возращение 12122022

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         ArrayList<Person> persons = new ArrayList<>();
         int maxNumLetSurname = 6;// максимальное колличество слов в фамилии
         persons.add(new Person("Ваня", "Иванов", 30));
-        persons.add(new Person("Сергей", "Митрофанов", 25));
+        persons.add(new Person("Сергей", "Митроф", 25));
         persons.add(new Person("Игорь", "Вавилов", 40));
         persons.add(new Person("Петр", "Зуев", 65));
         persons.add(new Person("Олег", "Цой", 10));
@@ -29,7 +29,7 @@ public class Main {
         persons.sort(comparison);
         System.out.println("Cравнение людей по принципу знатности");
         System.out.println("Список знатных людей:");
-        persons.removeIf(person -> person.getAge()<18);
+        persons.removeIf(person -> person.getAge() < 18);
         for (int i = 0; i < persons.size(); i++) {
             System.out.println(persons.get(i));
         }
---
+++ src/PersonsSurnameLengthComparator.java
@@ -0,0 +1,25 @@
+import java.util.Comparator;
+
+//public class PersonsSurnameLengthComparator implements Comparator<Person> {
+//    protected int maxNumberLettersSurname;
+//
+//    public PersonsSurnameLengthComparator(int maxNumberLettersSurname ) {
+//        this.maxNumberLettersSurname = maxNumberLettersSurname;
+//    }
+//    @Override
+//    public int compare(Person o1, Person o2) {
+//        if (o1.getSurname().length() > maxNumberLettersSurname - 1 && o2.getSurname().length() < maxNumberLettersSurname) {
+//            return -1;
+//        } else if (o1.getSurname().length() < maxNumberLettersSurname && o2.getSurname().length() > maxNumberLettersSurname - 1) {
+//            return 1;
+//        } else if (o1.getSurname().length() > maxNumberLettersSurname - 1 && o2.getSurname().length() > maxNumberLettersSurname - 1) {
+//            return Integer.compare(o2.getAge(), o1.getAge());
+//        } else if (o1.getSurname().length() > o2.getSurname().length()) {
+//            return -1;
+//        } else if (o1.getSurname().length() < o2.getSurname().length()) {
+//            return 1;
+//        } else {
+//            return Integer.compare(o2.getAge(), o1.getAge());
+//        }
+//    }
+//}
